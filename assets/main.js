2d821812b53d562a4323a9c62f1708c48f636201
The Dude <thedude@Erics-MacBook-Pro.local>
2019-03-16 11:45:33 -0700

    landing

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
     console.log("This is only a test");
 });
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyChXQ8ezEKei-S6Q1ggciPxdKYKLbO7QZg",
+    authDomain: "p1-looking-to-jam.firebaseapp.com",
+    databaseURL: "https://p1-looking-to-jam.firebaseio.com",
+    projectId: "p1-looking-to-jam",
+    storageBucket: "p1-looking-to-jam.appspot.com",
+    messagingSenderId: "618958973185"
+  };
+  firebase.initializeApp(config);
